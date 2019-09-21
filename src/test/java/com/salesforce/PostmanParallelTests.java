38e0c696bf12045ca9caa256e44aecbe6a70dcd4
gil127 <gildadosh@gmail.com>
2019-09-21 19:25:52 +0300

    add http async client

--- src/test/java/com/salesforce/PostmanParallelTests.java
+++ src/test/java/com/salesforce/PostmanParallelTests.java
@@ -4,6 +4,7 @@ import com.salesforce.http.clients.PostmanClient;
 import com.salesforce.http.clients.PtsvClient;
 import com.salesforce.utils.ThreadUtil;
 import org.apache.log4j.Logger;
+import org.asynchttpclient.*;
 import org.junit.Before;
 import org.junit.Test;
 import java.util.List;
@@ -38,5 +39,45 @@ public class PostmanParallelTests extends BaseTests {
         }
 
         logger.info(sb.toString());
+    }
+
+    @Test
+    public void submitTaskAsyncCalls() throws ExecutionException, InterruptedException {
+        String[] result = new String[4];
+        String[] requestParams = {"http://ptsv2.com/t/datoerror/post", "http://ptsv2.com/t/datoerror1/post",
+                "http://ptsv2.com/t/datoerror2/post", "http://ptsv2.com/t/datoerror3/post"};
+        AsyncHttpClient client = Dsl.asyncHttpClient();
+        List<ListenableFuture<Object>> executables = new ArrayList<>();
+
+        for (int i = 0; i < requestParams.length; i++) {
+            executables.add(client.preparePost(requestParams[i]).execute(asyncStringOrderHandler(result, i)));
+        }
+
+        for (int i = 0; i < executables.size(); i++) {
+            executables.get(0).get();
+        }
+
+        logger.info(buildStringFromArray(result));
+    }
+
+    private AsyncHandler<Object> asyncStringOrderHandler(String[] result, int i) {
+        AsyncCompletionHandler<Object> asyncCompletionHandler = new AsyncCompletionHandler<Object>() {
+            @Override
+            public Object onCompleted(Response response) {
+                result[i] = response.getResponseBody();
+                return response;
+            }
+        };
+
+        return asyncCompletionHandler;
+    }
+
+    private String buildStringFromArray(String[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+        }
+
+        return sb.toString();
     }
 }
